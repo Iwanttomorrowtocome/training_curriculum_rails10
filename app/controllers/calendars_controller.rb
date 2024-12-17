bf679d1fd1add32d48681a59befb9dd76e3f02ce
Iwanttomorrowtocome <white54brack@yahoo.co.jp>
2024-12-17 16:14:50 +0900

    isseu4-6解消２

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -38,7 +38,6 @@ class CalendarsController < ApplicationController
       end
 
       days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans }
-　　　　 main
       @week_days.push(days)
     end
   end
